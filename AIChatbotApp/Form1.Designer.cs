3abf50daf7ec667e30a6fc3cad203fccead657ef
M. Ammar Hashmi <ammarmuhammad1122@gmail.com>
2026-04-21 02:00:14 +0500

    Initial commit

---
+++ AIChatbotApp/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace AIChatbotApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbChat = new System.Windows.Forms.RichTextBox();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtbChat
+            // 
+            this.rtbChat.Location = new System.Drawing.Point(12, 12);
+            this.rtbChat.Name = "rtbChat";
+            this.rtbChat.Size = new System.Drawing.Size(602, 296);
+            this.rtbChat.TabIndex = 0;
+            this.rtbChat.Text = "";
+            this.rtbChat.TextChanged += new System.EventHandler(this.rtbChat_TextChanged);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(12, 315);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(521, 20);
+            this.txtInput.TabIndex = 1;
+            // 
+            // btnSend
+            // 
+            this.btnSend.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnSend.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnSend.Location = new System.Drawing.Point(539, 315);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = false;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(626, 352);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.rtbChat);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbChat;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ AIChatbotApp/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace AIChatbotApp
+{
+    public partial class Form1 : Form
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        // 🔑 API KEY HERE
+        string apiKey = "[ENCRYPTION_KEY]";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnSend_Click(object sender, EventArgs e)
+        {
+            string userInput = txtInput.Text;
+
+            if (string.IsNullOrWhiteSpace(userInput))
+                return;
+
+            rtbChat.AppendText("You: " + userInput + "\n");
+            txtInput.Clear();
+
+            try
+            {
+                string response = await GetAIResponse(userInput);
+                rtbChat.AppendText("AI: " + response + "\n\n");
+            }
+            catch (Exception ex)
+            {
+                rtbChat.AppendText("Error: " + ex.Message + "\n\n");
+            }
+
+            rtbChat.SelectionStart = rtbChat.Text.Length;
+            rtbChat.ScrollToCaret();
+        }
+
+        private async Task<string> GetAIResponse(string prompt)
+        {
+            string url = $"https://generativelanguage.googleapis.com/v1/models/gemini-2.5-flash:generateContent?key={apiKey}";
+
+            var requestBody = new
+            {
+                contents = new[]
+                {
+            new
+            {
+                parts = new[]
+                {
+                    new { text = prompt }
+                }
+            }
+        }
+            };
+
+            var json = JsonConvert.SerializeObject(requestBody);
+
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync(url, content);
+
+            string responseString = await response.Content.ReadAsStringAsync();
+
+          
+
+
+            dynamic result = JsonConvert.DeserializeObject(responseString);
+
+            // 🔥 SAFE CHECK
+            if (result.candidates == null)
+                throw new Exception("No candidates returned:\n" + responseString);
+
+            return result.candidates[0].content.parts[0].text.ToString();
+        }
+        private void rtbChat_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
